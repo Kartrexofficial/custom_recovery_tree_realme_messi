956f88f852ddaa100d1a9ddc1ad1e6bfe5ffd79f
KarthiK <172979966+Kartrexofficial@users.noreply.github.com>
2026-06-05 14:23:49 +0530

    Update and rename twrp_messi.mk to omni_messi.mk
    Signed-off-by: KarthiK <172979966+Kartrexofficial@users.noreply.github.com>

--- omni_messi.mk
+++ omni_messi.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common twrp stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/orangefox/config/common.mk)
 
 # Inherit from messi device
 $(call inherit-product, device/realme/messi/device.mk)
