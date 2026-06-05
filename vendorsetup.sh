85ec85322ad656164fcf9ac760fe861c13c7c5ff
KarthiK <172979966+Kartrexofficial@users.noreply.github.com>
2026-06-05 16:33:12 +0530

    Update vendorsetup.sh
    Signed-off-by: KarthiK <172979966+Kartrexofficial@users.noreply.github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -43,7 +43,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
   
 # Maintaining Info
     export OF_MAINTAINER=KartreXofficial
-    export FOX_MAINTAINER_PATCH_VERSION="R11.1"
+    export FOX_MAINTAINER_PATCH_VERSION="1"
     export FOX_BUILD_TYPE=beta
   	
 # Device Info
